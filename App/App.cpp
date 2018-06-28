438b39b41ae1929fdbeef40c3083fb57ad468cef
WowSoLaggy <dohxehapo@gmail.com>
2018-06-29 00:30:29 +0300

    (+) Added App project

---
+++ App/App.cpp
@@ -0,0 +1,26 @@
+#include "stdafx.h"
+#include "App.h"
+
+
+void App::run()
+{
+  initialize();
+  mainloop();
+  dispose();
+}
+
+
+void App::initialize()
+{
+}
+
+void App::mainloop()
+{
+  while (true)
+  {
+  }
+}
+
+void App::dispose()
+{
+}
---
+++ App/App.h
@@ -0,0 +1,16 @@
+#pragma once
+
+
+class App
+{
+public:
+
+  void run();
+
+private:
+
+  void initialize();
+  void mainloop();
+  void dispose();
+
+};
