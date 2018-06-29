45296d4e326455b7ace127131d569da2679c98c3
WowSoLaggy <dohxehapo@gmail.com>
2018-06-29 23:41:22 +0300

    (+) Added 'SettingsController'

---
+++ App/SettingsController.cpp
@@ -0,0 +1,19 @@
+#include "stdafx.h"
+#include "SettingsController.h"
+
+
+std::string SettingsController::getAppName() const
+{
+  return "Glory of Rome";
+}
+
+
+int SettingsController::getWindowWidth() const
+{
+  return 1280;
+}
+
+int SettingsController::getWindowHeight() const
+{
+  return 960;
+}
---
+++ App/SettingsController.h
@@ -0,0 +1,13 @@
+#pragma once
+
+
+class SettingsController
+{
+public:
+
+  std::string getAppName() const;
+
+  int getWindowWidth() const;
+  int getWindowHeight() const;
+
+};
