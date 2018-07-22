9a4cd3f57168ad68a4b1d78a5cd4beb5b60f0bc2
WowSoLaggy <dohxehapo@gmail.com>
2018-07-22 21:49:32 +0300

    (+) Added ObjectVm

---
+++ ViewModel/ObjectVm.cpp
@@ -0,0 +1,20 @@
+#include "stdafx.h"
+#include "ObjectVm.h"
+
+#include <Model/Object.h>
+#include <RenderApi/IResourceController.h>
+
+
+ObjectVm::ObjectVm(const IResourceController& i_resourceController, const Object& i_object)
+  : d_object(i_object)
+  , d_meshResourceId(i_resourceController.getResourceId(d_object.getModelName()))
+  , d_textureResourceId(i_resourceController.getResourceId(d_object.getTextureName()))
+{
+}
+
+
+Vector3 ObjectVm::getPosition() const
+{
+  const auto& objectPosition = d_object.getPosition();
+  return { objectPosition.x, objectPosition.y, 0 };
+}
---
+++ ViewModel/ObjectVm.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <Model/ModelFwd.h>
+#include <RenderApi/IObject3D.h>
+#include <RenderApi/RenderApiFwd.h>
+
+
+class ObjectVm : public IObject3D
+{
+public:
+
+  ObjectVm(const IResourceController& i_resourceController, const Object& i_object);
+
+  virtual ResourceId getMeshResourceId() const override { return d_meshResourceId; }
+  virtual ResourceId getTextureResourceId() const override { return d_textureResourceId; }
+
+  virtual Vector3 getPosition() const override;
+
+private:
+
+  const Object& d_object;
+
+  const ResourceId d_meshResourceId;
+  const ResourceId d_textureResourceId;
+};
