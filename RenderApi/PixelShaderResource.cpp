7e1e3d82ebd959cbd4ff37266430197832c10bd9
WowSoLaggy <dohxehapo@gmail.com>
2018-07-23 00:24:13 +0300

    (+) Added Sampler to 'PixelShaderResource'

--- RenderApi/PixelShaderResource.cpp
+++ RenderApi/PixelShaderResource.cpp
@@ -16,6 +16,8 @@ void PixelShaderResource::load(IRenderDevice& i_renderDevice)
 {
   auto& renderDevice = dynamic_cast<RenderDevice&>(i_renderDevice);
 
+  // Pixel Shader
+
   ID3D10Blob* pixelShaderBuffer = nullptr;
   D3DReadFileToBlob(Utils::getWString(d_shaderFilePath).c_str(), &pixelShaderBuffer);
 
@@ -23,9 +25,29 @@ void PixelShaderResource::load(IRenderDevice& i_renderDevice)
     pixelShaderBuffer->GetBufferSize(), NULL, &d_pixelShader);
 
   pixelShaderBuffer->Release();
+
+  // Sampler state
+
+  D3D11_SAMPLER_DESC samplerDesc;
+  samplerDesc.Filter = D3D11_FILTER_MIN_MAG_MIP_LINEAR;
+  samplerDesc.AddressU = D3D11_TEXTURE_ADDRESS_WRAP;
+  samplerDesc.AddressV = D3D11_TEXTURE_ADDRESS_WRAP;
+  samplerDesc.AddressW = D3D11_TEXTURE_ADDRESS_WRAP;
+  samplerDesc.MipLODBias = 0.0f;
+  samplerDesc.MaxAnisotropy = 1;
+  samplerDesc.ComparisonFunc = D3D11_COMPARISON_ALWAYS;
+  samplerDesc.BorderColor[0] = 0;
+  samplerDesc.BorderColor[1] = 0;
+  samplerDesc.BorderColor[2] = 0;
+  samplerDesc.BorderColor[3] = 0;
+  samplerDesc.MinLOD = 0;
+  samplerDesc.MaxLOD = D3D11_FLOAT32_MAX;
+
+  renderDevice.getDevicePtr()->CreateSamplerState(&samplerDesc, &d_sampleState);
 }
 
 void PixelShaderResource::unload()
 {
+  d_sampleState->Release();
   d_pixelShader->Release();
 }
--- RenderApi/PixelShaderResource.h
+++ RenderApi/PixelShaderResource.h
@@ -12,9 +12,13 @@ public:
   virtual void load(IRenderDevice& i_renderDevice) override;
   virtual void unload() override;
 
+  ID3D11PixelShader* getPtr() const { return d_pixelShader; }
+  ID3D11SamplerState* getSampleStatePtr() const { return d_sampleState; }
+
 private:
 
   const std::string d_shaderFilePath;
 
   ID3D11PixelShader* d_pixelShader;
+  ID3D11SamplerState* d_sampleState;
 };
