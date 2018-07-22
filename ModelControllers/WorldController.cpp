dfd4cc458c204eb5db608a46fe1b762f22b52982
WowSoLaggy <dohxehapo@gmail.com>
2018-07-22 23:30:38 +0300

    (*) Add models and textures to world model

--- ModelControllers/WorldController.cpp
+++ ModelControllers/WorldController.cpp
@@ -8,12 +8,16 @@ std::shared_ptr<World> WorldController::createNewWorld()
 {
   auto* pWorld = new World();
 
+  pWorld->getTerrain().setModelName("Surface.obj");
+  pWorld->getTerrain().setTextureName("Grass.dds");
   pWorld->getTerrain().setSizeX(10);
   pWorld->getTerrain().setSizeZ(10);
   pWorld->getTerrain().setHeight(0);
 
   Object player;
   player.setPosition({ 5, 5 });
+  player.setModelName("Player.obj");
+  player.setTextureName("Blank.dds");
   pWorld->getObjects().push_back(std::move(player));
 
   return std::shared_ptr<World>(pWorld);
