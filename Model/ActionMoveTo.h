ffda1848207afb6ee31c2503aa48cbcd73d0732e
WowSoLaggy <dohxehapo@gmail.com>
2018-09-30 22:05:21 +0300

    (*) Added tolerance to ActionMoveTo

--- Model/ActionMoveTo.h
+++ Model/ActionMoveTo.h
@@ -8,13 +8,19 @@
 class ActionMoveTo: public IAction
 {
 public:
-  ActionMoveTo(Vector2 i_goal)
+  ActionMoveTo(Vector2 i_goal, float i_tolerance = 0.5f)
     : d_goal(std::move(i_goal))
+    , d_tolerance(i_tolerance)
+    , d_toleranceSq(i_tolerance * i_tolerance)
   {
   }
 
   const Vector2& getGoal() const { return d_goal; }
+  float getTolerance() const { return d_tolerance; }
+  float getToleranceSq() const { return d_toleranceSq; }
 
 private:
   Vector2 d_goal;
+  float d_tolerance;
+  float d_toleranceSq;
 };
