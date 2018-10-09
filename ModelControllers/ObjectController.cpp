86fe1a05df93bb43116a06f3eedf481194ac7d8e
WowSoLaggy <dohxehapo@gmail.com>
2018-10-09 23:44:09 +0300

    (+) Tom now looks toward the movement direction

--- ModelControllers/ObjectController.cpp
+++ ModelControllers/ObjectController.cpp
@@ -18,7 +18,9 @@ void ObjectController::updateObject(Object& io_object, double i_dt)
     Vector2 position2 = { position.x, position.z };
     auto goal = pMoveToAction->getGoal();
 
-    if (lengthSq(goal - position2) <= pMoveToAction->getToleranceSq())
+    auto direction = goal - position2;
+
+    if (lengthSq(direction) <= pMoveToAction->getToleranceSq())
     {
       double timeToHold = (double)(std::rand() % 50) / 10;
       io_object.setCurrentAction(std::make_shared<ActionHold>(ActionHold(timeToHold)));
@@ -26,9 +28,10 @@ void ObjectController::updateObject(Object& io_object, double i_dt)
     }
 
     const float maxSpeed = 1.0f;
-    auto movement = normalize(goal - position2) * maxSpeed * (float)i_dt;
+    auto movement = normalize(direction) * maxSpeed * (float)i_dt;
 
     io_object.setPosition(io_object.getPosition() + Vector3{ movement.x, 0, movement.y });
+    io_object.setRotation({ 0, -std::atan2(direction.y, direction.x), 0 });
   }
   else if (auto* pHoldAction = dynamic_cast<ActionHold*>(pAction.get()))
   {
