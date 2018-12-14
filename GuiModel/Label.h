a4fcd57900a5f99cbead0feceb40c0df21a24e86
WowSoLaggy <dohxehapo@gmail.com>
2018-12-14 23:32:15 +0300

    (+) Added position to Gui models

--- GuiModel/Label.h
+++ GuiModel/Label.h
@@ -2,6 +2,8 @@
 
 #include "IGui.h"
 
+#include <Sdk/Vector.h>
+
 
 class Label : public IGui
 {
@@ -9,14 +11,18 @@ public:
 
   Label();
 
+  void setPosition(Vector2 i_position) { d_position = std::move(i_position); }
+  const Vector2& getPosition() const { return d_position; }
+
   const std::string& getFontName() const { return d_fontName; }
 
-  const std::string& getText() const { return d_text; }
   void setText(std::string i_text) { d_text = std::move(i_text); }
+  const std::string& getText() const { return d_text; }
 
 private:
 
+  Vector2 d_position;
+
   const std::string d_fontName;
   std::string d_text;
-
 };
--- GuiModel/Panel.h
+++ GuiModel/Panel.h
@@ -2,6 +2,8 @@
 
 #include "IGui.h"
 
+#include <Sdk/Vector.h>
+
 
 class Panel : public IGui
 {
@@ -9,9 +11,12 @@ public:
 
   Panel();
 
+  void setPosition(Vector2 i_position) { d_position = std::move(i_position); }
+  const Vector2& getPosition() const { return d_position; }
   const std::string& getTextureName() const { return d_textureName; }
 
 private:
 
   const std::string d_textureName;
+  Vector2 d_position;
 };
