2a1780caa6f2855497b4dc4c96a9e2f33640c880
WowSoLaggy <dohxehapo@gmail.com>
2018-12-15 22:48:33 +0300

    (~) Added GUI events

---
+++ GuiController/GuiEvents.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <GuiModel/GuiModelFwd.h>
+#include <Sdk/IEvent.h>
+
+
+class GuiAddedEvent : public IEvent
+{
+public:
+
+  GuiAddedEvent(IGuiPtr i_gui)
+    : d_gui(i_gui)
+  {
+  }
+
+  IGuiPtr getGui() const { return d_gui; }
+
+private:
+
+  IGuiPtr d_gui;
+};
+
+
+class GuiDeletedEvent : public IEvent
+{
+public:
+
+  GuiDeletedEvent(IGuiPtr i_gui)
+    : d_gui(i_gui)
+  {
+  }
+
+  IGuiPtr getGui() const { return d_gui; }
+
+private:
+
+  IGuiPtr d_gui;
+};
+
