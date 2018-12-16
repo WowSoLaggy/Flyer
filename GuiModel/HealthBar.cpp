b236bb117b151dd12d452b1242f15a9d67fe2220
WowSoLaggy <dohxehapo@gmail.com>
2018-12-16 16:22:44 +0300

    (+) HealthBar dynamically shows health

--- GuiModel/HealthBar.cpp
+++ GuiModel/HealthBar.cpp
@@ -7,7 +7,24 @@ HealthBar::HealthBar()
   , d_frontOffset({ 0, 0 })
   , d_sizeBack({ 0, 0 })
   , d_sizeFront({ 0, 0 })
+  , d_value(1)
   , d_textureBackName("")
   , d_textureFrontName("")
 {
+}
+
+
+Vector2 HealthBar::getPositionBack() const
+{
+  return d_position + d_offset;
+}
+
+Vector2 HealthBar::getPositionFront() const {
+  return d_position + d_offset + d_frontOffset;
+}
+
+
+Vector2 HealthBar::getSizeFront() const
+{
+  return { (float)(d_sizeFront.x * d_value), d_sizeFront.y };
 }
--- GuiModel/HealthBar.h
+++ GuiModel/HealthBar.h
@@ -15,14 +15,16 @@ public:
   void setOffset(Vector2 i_offset) { d_offset = std::move(i_offset); }
   void setFrontOffset(Vector2 i_frontOffset) { d_frontOffset = std::move(i_frontOffset); }
 
-  Vector2 getPositionBack() const { return d_position + d_offset; }
-  Vector2 getPositionFront() const { return d_position + d_offset + d_frontOffset; }
+  Vector2 getPositionBack() const;
+  Vector2 getPositionFront() const;
 
   void setSizeBack(Vector2 i_sizeBack) { d_sizeBack = std::move(i_sizeBack); }
   const Vector2& getSizeBack() const { return d_sizeBack; }
 
   void setSizeFront(Vector2 i_sizeFront) { d_sizeFront = std::move(i_sizeFront); }
-  const Vector2& getSizeFront() const { return d_sizeFront; }
+  Vector2 getSizeFront() const;
+
+  void setValue(double i_value) { d_value = i_value; }
 
   void setTextureBackName(std::string i_textureBackName) { d_textureBackName = std::move(i_textureBackName); }
   const std::string& getTextureBackName() const { return d_textureBackName; }
@@ -38,6 +40,8 @@ private:
 
   Vector2 d_sizeBack;
   Vector2 d_sizeFront;
+
+  double d_value;
 
   std::string d_textureBackName;
   std::string d_textureFrontName;
