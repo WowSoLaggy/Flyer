3faf776da52d22dd75ec6caa95652d41863d20fe
WowSoLaggy <dohxehapo@gmail.com>
2019-01-12 00:59:24 +0300

    (~) Added some utility funtions to 'ObjectProperty'

--- Model/ObjectProperty.h
+++ Model/ObjectProperty.h
@@ -7,20 +7,51 @@ class ObjectProperty
 public:
 
   ObjectProperty(T i_initialValue = T(0), T i_minValue = T(0), T i_maxValue = T(0))
-    : d_value(i_initialValue)
-    , d_valueMin(i_minValue)
-    , d_valueMax(i_maxValue)
   {
+    set(i_initialValue, i_minValue, i_maxValue);
   }
 
-  void setValue(T i_value) { d_value = i_value; }
+  void set(T i_value = T(0), T i_minValue = T(0), T i_maxValue = T(0))
+  {
+    setValueMin(i_minValue);
+    setValueMax(i_maxValue);
+    setValue(i_value);
+  }
+
+  void setToMin()
+  {
+    d_value = d_valueMin;
+  }
+
+  void setToMax()
+  {
+    d_value = d_valueMax;
+  }
+
   T getValue() const { return d_value; }
+  void setValue(T i_value)
+  {
+    d_value =
+      d_value < d_valueMin ? d_valueMin :
+      d_value > d_valueMax ? d_valueMax :
+      i_value;
+  }
 
-  void setValueMin(T i_valueMin) { d_valueMin = i_valueMin; }
   T getValueMin() const { return d_valueMin; }
+  void setValueMin(T i_valueMin)
+  {
+    d_valueMin = i_valueMin;
+    if (d_value < d_valueMin)
+      d_value = d_valueMin;
+  }
 
-  void setValueMax(T i_valueMax) { d_valueMax = i_valueMax; }
   T getValueMax() const { return d_valueMax; }
+  void setValueMax(T i_valueMax)
+  {
+    d_valueMax = i_valueMax;
+    if (d_value > d_valueMax)
+      d_value = d_valueMax;
+  }
 
   double getValueRelative() const { return double(d_value - d_valueMin) / double(d_valueMax - d_valueMin); }
 
