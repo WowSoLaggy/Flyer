f53d759d4ca9a3988845dce7e4883db3ded61970
WowSoLaggy <dohxehapo@gmail.com>
2019-01-12 00:16:07 +0300

    (+) 'ActionMoveTo' now supports moving to object

--- Model/ActionMoveTo.h
+++ Model/ActionMoveTo.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include "IAction.h"
+#include "ModelFwd.h"
+#include "Object.h"
 
 #include <Sdk/Vector.h>
 
@@ -8,6 +10,13 @@
 class ActionMoveTo : public IAction
 {
 public:
+  ActionMoveTo(ObjectPtr i_goalObject, float i_tolerance = 0.5f)
+    : d_goalObject(i_goalObject)
+    , d_tolerance(i_tolerance)
+    , d_toleranceSq(i_tolerance * i_tolerance)
+  {
+  }
+
   ActionMoveTo(Vector2 i_goal, float i_tolerance = 0.5f)
     : d_goal(std::move(i_goal))
     , d_tolerance(i_tolerance)
@@ -17,11 +26,12 @@ public:
 
   virtual ActionType getActionType() const override { return ActionType::MoveTo; }
 
-  const Vector2& getGoal() const { return d_goal; }
+  Vector2 getGoal() const { return d_goalObject ? xyz2xz(d_goalObject->getPosition()) : d_goal; }
   float getTolerance() const { return d_tolerance; }
   float getToleranceSq() const { return d_toleranceSq; }
 
 private:
+  ObjectPtr d_goalObject;
   Vector2 d_goal;
   float d_tolerance;
   float d_toleranceSq;
