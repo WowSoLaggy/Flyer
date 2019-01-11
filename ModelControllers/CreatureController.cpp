75427dfd9ed4c91db464c1a689cc29fb106e7883
WowSoLaggy <dohxehapo@gmail.com>
2019-01-12 00:18:47 +0300

    (=) Removed unused code

--- ModelControllers/CreatureController.cpp
+++ ModelControllers/CreatureController.cpp
@@ -11,22 +11,10 @@
 
 namespace
 {
-  std::map<ObjectId, ObjectId> s_destinationArrowIdMap;
 
-  void createDestinationArrowObject(ObjectId& o_destinationArrowId, const Vector3& i_position,
-                                    WorldController& io_worldController)
   {
-    ObjectPtr arrow = std::make_shared<Object>();
-    arrow->setPosition(i_position);
-    arrow->setModelName("Arrow.cmo");
 
-    o_destinationArrowId = arrow->getId();
-    io_worldController.addObject(arrow);
-  }
 
-  void deleteDestinationArrowObject(ObjectId i_destinationArrowId, WorldController& io_worldController)
-  {
-    io_worldController.deleteObject(i_destinationArrowId);
   }
 
 } // anonymous NS
@@ -38,20 +26,8 @@ void CreatureController::updateObject(CreaturePtr io_creature, double i_dt,
   auto& action = io_creature->getCurrentAction();
   switch (action.getActionType())
   {
-  /*case ActionType::Hold:
   {
-    auto& holdAction = dynamic_cast<ActionHold&>(action);
-    if (!holdAction.hold(i_dt))
-    {
-      float newX = (float)(std::rand() % 100 + 20) / 10;
-      float newY = (float)(std::rand() % 160 + 20) / 10;
-      io_creature->setCurrentAction(std::make_shared<ActionMoveTo>(ActionMoveTo({ newX, newY })));
-      
-      createDestinationArrowObject(s_destinationArrowIdMap[io_creature->getId()], { newX, 1, newY },
-                                   io_worldController);
-    }
     break;
-  }*/
 
   case ActionType::MoveTo:
   {
@@ -66,9 +42,6 @@ void CreatureController::updateObject(CreaturePtr io_creature, double i_dt,
     {
       double timeToHold = (double)(std::rand() % 50) / 10;
       io_creature->setCurrentAction(std::make_shared<ActionIdle>());
-
-      //deleteDestinationArrowObject(s_destinationArrowIdMap.at(io_creature->getId()), io_worldController);
-
       return;
     }
 
@@ -81,5 +54,5 @@ void CreatureController::updateObject(CreaturePtr io_creature, double i_dt,
     break;
   }
 
-  } // switch
+  } // switch (action.getActionType())
 }
