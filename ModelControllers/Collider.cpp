416a6657b3c2ae30de16f2449ea2ed8c5153100b
WowSoLaggy <dohxehapo@gmail.com>
2019-01-13 11:29:00 +0300

    (~) 'Collider' now returns normal and tangent vectors

--- ModelControllers/Collider.cpp
+++ ModelControllers/Collider.cpp
@@ -5,7 +5,8 @@
 #include <Model/IRealObject.h>
 
 
-bool Collider::collide(IRealObjectPtr i_object1, IRealObjectPtr i_object2)
+bool Collider::collide(IRealObjectPtr i_object1, IRealObjectPtr i_object2,
+                       Vector2& o_normal, Vector2& o_tangent)
 {
   const auto& shape1 = i_object1->getCollisionShape();
   const auto& shape2 = i_object2->getCollisionShape();
@@ -13,7 +14,8 @@ bool Collider::collide(IRealObjectPtr i_object1, IRealObjectPtr i_object2)
   if (shape1.getShapeType() == ShapeType::Circle && shape2.getShapeType() == ShapeType::Circle)
   {
     return collideCircle2Circle(dynamic_cast<const Circle&>(shape1), dynamic_cast<const Circle&>(shape2),
-                                xyz2xz(i_object1->getPosition()), xyz2xz(i_object2->getPosition()));
+                                xyz2xz(i_object1->getPosition()), xyz2xz(i_object2->getPosition()),
+                                o_normal, o_tangent);
   }
 
   throw std::runtime_error("Unexpected collision shapes.");
@@ -21,11 +23,20 @@ bool Collider::collide(IRealObjectPtr i_object1, IRealObjectPtr i_object2)
 
 
 bool Collider::collideCircle2Circle(const Circle& i_circle1, const Circle& i_circle2,
-                                    const Vector2& i_offset1, const Vector2& i_offset2)
+                                    const Vector2& i_offset1, const Vector2& i_offset2,
+                                    Vector2& o_normal, Vector2& o_tangent)
 {
-  double distanceCentersSq = lengthSq(i_offset2 - i_offset1);
+  auto distanceVectorReverse = i_offset1 - i_offset2;
+
+  double distanceCentersSq = lengthSq(distanceVectorReverse);
   double radiiSumSq = i_circle1.getRadius() + i_circle2.getRadius();
   radiiSumSq *= radiiSumSq;
 
-  return distanceCentersSq < radiiSumSq;
+  if (distanceCentersSq >= radiiSumSq)
+    return false;
+
+  o_normal = normalize(distanceVectorReverse);
+  o_tangent = { o_normal.y, -o_normal.x };
+
+  return true;
 }
--- ModelControllers/Collider.h
+++ ModelControllers/Collider.h
@@ -9,9 +9,11 @@ class Collider
 public:
   Collider() = delete;
 
-  static bool collide(IRealObjectPtr i_object1, IRealObjectPtr i_object2);
+  static bool collide(IRealObjectPtr i_object1, IRealObjectPtr i_object2,
+                      Vector2& o_normal, Vector2& o_tangent);
 
 private:
   static bool collideCircle2Circle(const Circle& i_circle1, const Circle& i_circle2,
-                                   const Vector2& i_offset1, const Vector2& i_offset2);
+                                   const Vector2& i_offset1, const Vector2& i_offset2,
+                                   Vector2& o_normal, Vector2& o_tangent);
 };
