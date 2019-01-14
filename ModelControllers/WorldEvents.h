c1fc10249f9d2ce5110bfbcc78beee6aa89bd671
WowSoLaggy <dohxehapo@gmail.com>
2019-01-15 00:47:50 +0300

    (~) Added 'ObjectChangedActionEvent'

--- ModelControllers/WorldEvents.h
+++ ModelControllers/WorldEvents.h
@@ -37,3 +37,23 @@ private:
   ObjectPtr d_object;
 };
 
+
+class ObjectChangedActionEvent : public IEvent
+{
+public:
+
+  ObjectChangedActionEvent(ObjectPtr i_object, IActionPtr i_previousAction)
+    : d_object(i_object)
+    , d_previousAction(i_previousAction)
+  {
+  }
+
+  ObjectPtr getObject() const { return d_object; }
+  IActionPtr getPreviousAction() const { return d_previousAction; }
+
+private:
+
+  ObjectPtr d_object;
+  IActionPtr d_previousAction;
+
+};
