01aa324f8e7d905878333cde017aa037845c9365
WowSoLaggy <dohxehapo@gmail.com>
2019-02-01 00:15:05 +0300

    (-) 'Gui3dCreator' creates collision shapes of correct size

--- GuiController/Gui3dCreator.cpp
+++ GuiController/Gui3dCreator.cpp
@@ -3,6 +3,8 @@
 
 #include <GuiModel/CollisionShapeGui3d.h>
 #include <GuiModel/Gui3dCollection.h>
+#include <Model/Aabb.h>
+#include <Model/Circle.h>
 #include <Model/IShape.h>
 
 
@@ -12,18 +14,37 @@ std::shared_ptr<Gui3dCollection> Gui3dCreator::create()
 }
 
 
-std::shared_ptr<CollisionShapeGui3d> Gui3dCreator::createCollisionShapeGui3d(ShapeType i_shapeType)
+std::shared_ptr<CollisionShapeGui3d> Gui3dCreator::createCollisionShapeGui3d(const IShape& i_shape)
 {
   static const std::map<ShapeType, std::string> shapeTextureMap =
   {
     { ShapeType::Circle, "Circle.dds" },
     { ShapeType::Aabb, "Square.dds" },
   };
+  const auto shapeType = i_shape.getShapeType();
 
   auto collisionShapeGui3d = std::make_shared<CollisionShapeGui3d>();
 
   collisionShapeGui3d->setModelName("Tile.cmo");
-  collisionShapeGui3d->setTextureName(shapeTextureMap.at(i_shapeType));
+  collisionShapeGui3d->setTextureName(shapeTextureMap.at(shapeType));
+
+  switch (shapeType)
+  {
+  case ShapeType::Circle:
+  {
+    const auto& shape = dynamic_cast<const Circle&>(i_shape);
+    float scale = (float)shape.getRadius() * 2;
+    collisionShapeGui3d->setScale({ scale, 1.0f, scale });
+    break;
+  }
+
+  case ShapeType::Aabb:
+  {
+    const auto& shape = dynamic_cast<const Aabb&>(i_shape);
+    collisionShapeGui3d->setScale({ (float)shape.getWidth(), 1.0f, (float)shape.getHeight() });
+    break;
+  }
+  }
 
   return collisionShapeGui3d;
 }
--- GuiController/Gui3dCreator.h
+++ GuiController/Gui3dCreator.h
@@ -9,7 +9,7 @@ class Gui3dCreator
 public:
   static std::shared_ptr<Gui3dCollection> create();
 
-  static std::shared_ptr<CollisionShapeGui3d> createCollisionShapeGui3d(ShapeType i_shapeType);
+  static std::shared_ptr<CollisionShapeGui3d> createCollisionShapeGui3d(const IShape& i_shape);
 
 private:
   Gui3dCreator() = default;
