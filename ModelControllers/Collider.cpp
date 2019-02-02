8e8c6710083dbe6e7702411430bf8bea4d8efddc
WowSoLaggy <dohxehapo@gmail.com>
2019-02-02 22:59:56 +0300

    (+) Circle-rect collision test

--- ModelControllers/Collider.cpp
+++ ModelControllers/Collider.cpp
@@ -7,7 +7,7 @@
 
 
 bool Collider::collide(ObjectPtr i_object1, ObjectPtr i_object2,
-                       Vector2& o_normal, Vector2& o_tangent)
+                       Vector2& o_normal)
 {
   if (!i_object1->hasCollisionShape() || !i_object2->hasCollisionShape())
     return false;
@@ -20,7 +20,7 @@ bool Collider::collide(ObjectPtr i_object1, ObjectPtr i_object2,
     return collideCircle2Circle(dynamic_cast<const ColCircle&>(shape1).getCircle(),
                                 dynamic_cast<const ColCircle&>(shape2).getCircle(),
                                 xyz2xz(i_object1->getPosition()), xyz2xz(i_object2->getPosition()),
-                                o_normal, o_tangent);
+                                o_normal);
   }
   else if (shape1.getColShapeType() == ColShapeType::Rect && shape2.getColShapeType() == ColShapeType::Circle)
   {
@@ -28,7 +28,7 @@ bool Collider::collide(ObjectPtr i_object1, ObjectPtr i_object2,
                               dynamic_cast<const ColCircle&>(shape2).getCircle(),
                               xyz2xz(i_object1->getPosition()), xyz2xz(i_object2->getPosition()),
                               i_object1->getRotation().y,
-                              o_normal, o_tangent);
+                              o_normal);
   }
   else if (shape1.getColShapeType() == ColShapeType::Circle && shape2.getColShapeType() == ColShapeType::Rect)
   {
@@ -36,7 +36,7 @@ bool Collider::collide(ObjectPtr i_object1, ObjectPtr i_object2,
                               dynamic_cast<const ColRect&>(shape2).getRect(),
                               xyz2xz(i_object1->getPosition()), xyz2xz(i_object2->getPosition()),
                               i_object2->getRotation().y,
-                              o_normal, o_tangent);
+                              o_normal);
   }
   else if (shape1.getColShapeType() == ColShapeType::Rect && shape2.getColShapeType() == ColShapeType::Rect)
   {
@@ -44,7 +44,7 @@ bool Collider::collide(ObjectPtr i_object1, ObjectPtr i_object2,
                             dynamic_cast<const ColRect&>(shape2).getRect(),
                             xyz2xz(i_object1->getPosition()), xyz2xz(i_object2->getPosition()),
                             i_object1->getRotation().y, i_object2->getRotation().y,
-                            o_normal, o_tangent);
+                            o_normal);
   }
 
   throw std::runtime_error("Unexpected collision shapes.");
@@ -53,7 +53,7 @@ bool Collider::collide(ObjectPtr i_object1, ObjectPtr i_object2,
 
 bool Collider::collideCircle2Circle(const Circle& i_circle1, const Circle& i_circle2,
                                     const Vector2& i_offset1, const Vector2& i_offset2,
-                                    Vector2& o_normal, Vector2& o_tangent)
+                                    Vector2& o_normal)
 {
   auto distanceVectorReverse = i_offset1 - i_offset2;
 
@@ -65,7 +65,6 @@ bool Collider::collideCircle2Circle(const Circle& i_circle1, const Circle& i_cir
     return false;
 
   o_normal = normalize(distanceVectorReverse);
-  o_tangent = { o_normal.y, -o_normal.x };
 
   return true;
 }
@@ -73,23 +72,51 @@ bool Collider::collideCircle2Circle(const Circle& i_circle1, const Circle& i_cir
 bool Collider::collideRect2Circle(const Rect& i_rect, const Circle& i_circle,
                                   const Vector2& i_offset1, const Vector2& i_offset2,
                                   float i_rotation1,
-                                  Vector2& o_normal, Vector2& o_tangent)
+                                  Vector2& o_normal)
 {
-  return collideCircle2Rect(i_circle, i_rect, i_offset2, i_offset1, i_rotation1, o_normal, o_tangent);
+  return collideCircle2Rect(i_circle, i_rect, i_offset2, i_offset1, i_rotation1, o_normal);
 }
 
 bool Collider::collideCircle2Rect(const Circle& i_circle, const Rect& i_rect,
                                   const Vector2& i_offset1, const Vector2& i_offset2,
                                   float i_rotation2,
-                                  Vector2& o_normal, Vector2& o_tangent)
+                                  Vector2& o_normal)
 {
-  return false;
+  auto offset = i_offset1 - i_offset2;
+  offset = rotate(offset, -i_rotation2);
+
+  float distX = std::abs(offset.x);
+  float distY = std::abs(offset.y);
+
+  if (distX > (i_rect.getWidthHalf() + i_circle.getRadius()))
+    return false;
+  if (distY > (i_rect.getHeightHalf() + i_circle.getRadius()))
+    return false;
+
+  if (distX <= i_rect.getWidthHalf())
+  {
+    float unitValue = offset.y < 0 ? -1.0f : 1.0f;
+    o_normal = rotate({ 0.0f, unitValue }, i_rotation2);
+    return true;
+  }
+  if (distY <= i_rect.getHeightHalf())
+  {
+    float unitValue = offset.x < 0 ? -1.0f : 1.0f;
+    o_normal = rotate({ unitValue, 0.0f }, i_rotation2);
+    return true;
+  }
+
+  float cornerDistanceSq =
+    (distX - i_rect.getWidth() / 2) * (distX - i_rect.getWidth() / 2) +
+    (distY - i_rect.getHeight() / 2) * (distY - i_rect.getHeight() / 2);
+
+  return cornerDistanceSq <= i_circle.getRadiusSq();
 }
 
 bool Collider::collideRect2Rect(const Rect& i_rect1, const Rect& i_rect2,
                                 const Vector2& i_offset1, const Vector2& i_offset2,
                                 float i_rotation1, float i_rotation2,
-                                Vector2& o_normal, Vector2& o_tangent)
+                                Vector2& o_normal)
 {
   return false;
 }
--- ModelControllers/Collider.h
+++ ModelControllers/Collider.h
@@ -10,22 +10,22 @@ public:
   Collider() = delete;
 
   static bool collide(ObjectPtr i_object1, ObjectPtr i_object2,
-                      Vector2& o_normal, Vector2& o_tangent);
+                      Vector2& o_normal);
 
 private:
   static bool collideCircle2Circle(const Circle& i_circle1, const Circle& i_circle2,
                                    const Vector2& i_offset1, const Vector2& i_offset2,
-                                   Vector2& o_normal, Vector2& o_tangent);
+                                   Vector2& o_normal);
   static bool collideRect2Circle(const Rect& i_rect, const Circle& i_circle,
                                  const Vector2& i_offset1, const Vector2& i_offset2,
                                  float i_rotation1,
-                                 Vector2& o_normal, Vector2& o_tangent);
+                                 Vector2& o_normal);
   static bool collideCircle2Rect(const Circle& i_circle, const Rect& i_rect,
                                  const Vector2& i_offset1, const Vector2& i_offset2,
                                  float i_rotation2,
-                                 Vector2& o_normal, Vector2& o_tangent);
+                                 Vector2& o_normal);
   static bool collideRect2Rect(const Rect& i_rect1, const Rect& i_rect2,
                                const Vector2& i_offset1, const Vector2& i_offset2,
                                float i_rotation1, float i_rotation2,
-                               Vector2& o_normal, Vector2& o_tangent);
+                               Vector2& o_normal);
 };
